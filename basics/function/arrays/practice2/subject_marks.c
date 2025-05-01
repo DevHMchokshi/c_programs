14a3a6400483249e787d0a705c33495428960197
dev <devchokshi100@gmail.com>
2025-05-01 11:59:37 +0530

    added new test file

---
+++ basics/function/arrays/practice2/subject_marks.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int p1,p2,p3,p4;    
+printf("p1 of first class");
+scanf("%d",&p1);
+printf("p2 of second class");
+scanf("%d",p2);
+printf("percentage of pass class");
+scanf("%d",&p3);
+printf("percentage of fail class");
+scanf("%d",&p4);
+}       
---
+++ basics/function/if_else.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a=10,b=20;
+    if (a>b)
+    {
+        printf("a is big");
+    }
+    else
+    {
+        printf("b is big");
+    }
+}
